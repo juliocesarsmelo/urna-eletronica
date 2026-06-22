063021cadea8c4a813ac60b764663c90a63f02c7
juliocesarsmelo <juliocesarmelo.pro@gmail.com>
2026-06-22 03:27:27 -0300

    refatorar banco.py

--- database/banco.py
+++ database/banco.py
@@ -12,7 +12,7 @@ def criar_tabelas():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS candidatos (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        numero INTEGER UNIQUE,
+        numero INTEGER UNIQUE NOT NULL,
         nome TEXT NOT NULL,
         partido TEXT NOT NULL
     )
@@ -22,7 +22,7 @@ def criar_tabelas():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS eleitores (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        cpf TEXT UNIQUE,
+        cpf TEXT UNIQUE NOT NULL,
         nome TEXT NOT NULL,
         votou INTEGER DEFAULT 0
     )
@@ -32,8 +32,10 @@ def criar_tabelas():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS votos (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        eleitor_id INTEGER,
-        candidato_id INTEGER
+        eleitor_id INTEGER NOT NULL,
+        candidato_id INTEGER NOT NULL,
+        FOREIGN KEY(eleitor_id) REFERENCES eleitores(id),
+        FOREIGN KEY(candidato_id) REFERENCES candidatos(id)
     )
     """)
 
