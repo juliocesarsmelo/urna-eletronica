62aa27a26dd8a81d4468bb30edaff86e9fb7ac9d
juliocesarsmelo <juliocesarmelo.pro@gmail.com>
2026-06-22 03:08:31 -0300

    refatorar listagem de eleitores em eleitores.py

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
 from database.banco import criar_tabelas
-from candidatos import *
-from eleitores import *
-from votos import votar
+from candidatos import cadastrar_candidato, listar_candidatos, atualizar_candidato, excluir_candidato
+from eleitores import cadastrar_eleitor, listar_eleitores, atualizar_eleitor, excluir_eleitor
+from votos import votar, apurar_votos
 
-criar_tabelas()
+def menu():
+    criar_tabelas()
 
-for i in range(5):
-    cadastrar_candidato()
+    while True:
+        print("\n===== URNA ELETRÔNICA =====")
+        print("1 - Cadastrar candidato")
+        print("2 - Listar candidatos")
+        print("3 - Atualizar candidato")
+        print("4 - Excluir candidato")
+        print("5 - Cadastrar eleitor")
+        print("6 - Listar eleitores")
+        print("7 - Atualizar eleitor")
+        print("8 - Excluir eleitor")
+        print("9 - Votar")
+        print("10 - Apurar votos")
+        print("0 - Sair")
 
-listar_candidatos()
+        opcao = input("\nEscolha uma opção: ")
 
-atualizar_candidato()
+        if opcao == "1":
+            cadastrar_candidato()
+        elif opcao == "2":
+            listar_candidatos()
+        elif opcao == "3":
+            atualizar_candidato()
+        elif opcao == "4":
+            excluir_candidato()
+        elif opcao == "5":
+            cadastrar_eleitor()
+        elif opcao == "6":
+            listar_eleitores()
+        elif opcao == "7":
+            atualizar_eleitor()
+        elif opcao == "8":
+            excluir_eleitor()
+        elif opcao == "9":
+            votar()
+        elif opcao == "10":
+            apurar_votos()
+        elif opcao == "0":
+            print("\nEncerrando sistema... Até logo!")
+            break
+        else:
+            print("\nOpção inválida, tente novamente.")
 
-excluir_candidato()
+if __name__ == "__main__":
+    menu()
 
-listar_candidatos()
 
-cadastrar_eleitor()
 
-votar()
